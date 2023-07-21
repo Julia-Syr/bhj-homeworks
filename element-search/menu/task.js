41478c526fbb05bfff95afb626cae4a079c04021
Julia-Syr <boberia@mail.ru>
2023-07-21 19:58:19 +0300

    first commit menu

---
+++ element-search/menu/task.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll(".menu__link");
+links.forEach((element) => {
+    element.onclick = function () {
+        let parentLink = element.closest("menu__item");
+        let parentHasSubMenu = parentLink.querySelector(".sub__menu");
+        if (parentHasSubMenu) {
+            parentHasSubMenu.classList.toggle("menu_active");
+            return false;
+        }
+    }
+})
