735f59f87ab2f64c612d05b7f06c6c849660bd1b
Julia-Syr <boberia@mail.ru>
2023-07-22 13:41:59 +0300

    third commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,8 +2,8 @@ const links = document.querySelectorAll(".menu__link");
 links.forEach((element) => {
     element.onclick = function (event) {
         event.preventDefault();
-        let parentLink = element.closest("menu__item");
-        let parentHasSubMenu = parentLink.querySelector(".sub__menu");
+        let parentLink = element.closest(".menu__item");
+        let parentHasSubMenu = parentLink.querySelector(".menu_sub");
         if (parentHasSubMenu) {
             parentHasSubMenu.classList.toggle("menu_active");
             return false;
