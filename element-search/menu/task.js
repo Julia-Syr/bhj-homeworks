4b38adb664c7f6f4eb8d19bc1ce02b8c4247c214
Julia-Syr <boberia@mail.ru>
2023-07-22 11:05:28 +0300

    second commit menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,12 @@
 const links = document.querySelectorAll(".menu__link");
 links.forEach((element) => {
-    element.onclick = function () {
+    element.onclick = function (event) {
+        event.preventDefault();
         let parentLink = element.closest("menu__item");
         let parentHasSubMenu = parentLink.querySelector(".sub__menu");
         if (parentHasSubMenu) {
             parentHasSubMenu.classList.toggle("menu_active");
             return false;
-        }
+        } 
     }
 })
